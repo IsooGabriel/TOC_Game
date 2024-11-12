dbcd0136eb2aa8ee9a12d53ce31087dd1ea6c9ef
GabuGabuNoMi <139324502+IsooGabriel@users.noreply.github.com>
2024-11-12 14:58:28 +0900

    色々

---
+++ TOC_Game/Assets/Code/C#/PlayerMoveSystem_Gabu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using DG.Tweening;
+
+public class PlayerMoveSystem_Gabu : MonoBehaviour
+{
+    PlayerInput playerInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
