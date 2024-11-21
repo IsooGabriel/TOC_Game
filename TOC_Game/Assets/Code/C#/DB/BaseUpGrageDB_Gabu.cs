322d280b46f1449b08cc0031b95fafe1208dc938
GabuGabuNoMi <139324502+IsooGabriel@users.noreply.github.com>
2024-11-21 14:57:30 +0900

    DB更新

---
+++ TOC_Game/Assets/Code/C#/DB/BaseUpGrageDB_Gabu.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "CreateData/BaseUpGrage")]
+public class BaseUpGrageDB_Gabu : ScriptableObject
+{
+}
---
+++ TOC_Game/Assets/Code/C#/DB/DBManager_Gabu.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CreateData/DB")]
+public class DBManager_Gabu : ScriptableObject
+{
+    static DBManager_Gabu DBs;
+    public static DBManager_Gabu DB
+    {
+        get
+        {
+            if (DBs == null)
+            {
+                DBs = (DBManager_Gabu)Resources.Load("DataBase");
+            }
+            return DBs;
+        }
+    }
+
+    public EnemyDB_Gabu[] enemyDBs;
+    public EnemyDB_Gabu GetEnemyDB(int ID)
+    {
+        if (ID < 0)
+        {
+            Debug.LogError("IDは0以上である必要があります. " + ID);
+            return enemyDBs[0];
+        }
+        if (ID > enemyDBs.Length)
+        {
+            Debug.LogError("IDがデータベースの長さを超えています. " + ID);
+            return enemyDBs[0];
+        }
+        return enemyDBs[ID] == null ? enemyDBs[0] : enemyDBs[ID];
+    }
+
+
+}
