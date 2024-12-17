a7b641e698b37fdecc0d6db7185e76db37aeb2c9
GabuGabuNoMi <139324502+IsooGabriel@users.noreply.github.com>
2024-12-17 14:55:04 +0900

    TreeSystem書いてる

--- TOC_Game/Assets/Code/C#/Tree_UpGrage/TreeManager_Gabu.cs
+++ TOC_Game/Assets/Code/C#/Tree_UpGrage/TreeManager_Gabu.cs
@@ -1,6 +1,7 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static DBManager_Gabu;
 
 public class TreeManager_Gabu : MonoBehaviour
 {
@@ -14,11 +15,27 @@ public class TreeManager_Gabu : MonoBehaviour
     public Button unlickButton;
     // prefabに変更を加え複製する、
     // DBからデータを取得して、TreeElementSystem_Gabuの配列に格納する、解放の有無を確認する
-    private void Start()
+    private void Awake()
     {
-        foreach(var element in elements)
+        if (elementPrefab.GetComponent<TreeElementSystem_Gabu>() == null)
         {
+            Debug.LogWarning("TreeElementSystem_Gabuがアタッチされていません");
+            elementPrefab.AddComponent<TreeElementSystem_Gabu>();
+        }
 
+        foreach (var element in DB.baseUpGrageDBs)  // アップグレードをインスタンス
+        {
+            GameObject obj = Instantiate(elementPrefab, transform);
+            TreeElementSystem_Gabu tes = obj.GetComponent<TreeElementSystem_Gabu>();
+            obj.name = element.name;
+            obj.transform.localPosition = new Vector3(element.treePosition.x, element.treePosition.y, 0);
+            tes.image.sprite = element.prefab;
+            tes.image.color = element.color;
+            tes.baseUpGrageDB_Gabu = element;
+            if (DB.playerDBs[DB.AccountID].baseUpGrages[element.UpGrageID])
+            {
+
+            }
         }
     }
 
