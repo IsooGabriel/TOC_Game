7202927a603e80dabb6a4787773b130e7ab57e82
dankyv1 <158531713+dankyv1@users.noreply.github.com>
2025-01-20 12:06:33 +0900

    movimento追加
    プレイヤーの動きを実装

--- TOC_Game/Assets/Yudi-Folder/movimento.cs
+++ TOC_Game/Assets/Yudi-Folder/movimento.cs
@@ -1,29 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class movimento : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float smoothTime = 0.1f; // Smoothing factor
 
-    private Vector2 movement;
+    private Vector2 movement; // Input movement vector
     private Vector2 velocity = Vector2.zero;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+
+    private PlayerInput playerInput; // Reference to PlayerInput component
+
+    // Called when the object is created
     void Start()
     {
-        
+        // Get the PlayerInput component attached to the player GameObject
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    // This method is called automatically by the Input System when input changes
+    public void Move(InputAction.CallbackContext context)
+    {
+        // Read the movement input as a Vector2
+        movement = context.ReadValue<Vector2>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get player input for movement
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        // Smoothly move the player based on the input vector
+        MovePlayer();
+    }
 
-        // Normalize the movement vector
-        movement = new Vector2(moveX, moveY).normalized;
-
-        // Smoothly move the player
+    // Method to handle player movement
+    void MovePlayer()
+    {
+        // Smoothly transition between current and target position
         transform.position = Vector2.SmoothDamp(transform.position, transform.position + (Vector3)movement, ref velocity, smoothTime, moveSpeed, Time.deltaTime);
     }
 }
+
